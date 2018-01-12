72e8f7423a158f28a9543c86d4760011c6311d55
Ian Crandell <ian85@vbi.vt.edu>
2018-01-12 19:08:22 +0000

    Changed to summarize level instead of importance

--- src/STEM_jorbs/ONET_ksa.R
+++ src/STEM_jorbs/ONET_ksa.R
@@ -1,8 +1,14 @@
+library(dplyr)
+library(reshape2)
+
 # Get KSA data for each profession
 
-knowledge = read.table("Knowledge.txt", header = T, sep = "\t")
-skills = read.table("Skills.txt", header = T, sep = "\t")
-abilities = read.table("Abilities.txt", header = T, sep = "\t")
+data.dir= "./data/stem_edu/original/OPED_Database_22/"
+
+
+knowledge = read.table(paste0(data.dir, "Knowledge.txt"), header = T, sep = "\t")
+skills = read.table(paste0(data.dir, "Skills.txt"), header = T, sep = "\t")
+abilities = read.table(paste0(data.dir, "Abilities.txt"), header = T, sep = "\t")
 
 knowledge %>% transmute(O.NET.SOC.Code, Element.Name, Scale.ID = paste(Scale.ID, "knowledge", sep = "_"), Data.Value) %>%
   cast(O.NET.SOC.Code ~ Scale.ID + Element.Name) -> k1
@@ -13,15 +19,14 @@ abilities %>% transmute(O.NET.SOC.Code, Element.Name, Scale.ID = paste(Scale.ID,
 
 ksa = merge(merge(k1, s1), a1)
 
-ksaPca = cmdscale(dist(scale(ksa[,-1])))
 
 
 ## Extract the 6 stem knowledge fields for all professions: Biology, Chemistry, Physics, Computers and Electronics, Engineering and Technology, mathematics
 
 knowledge %>% filter(Element.Name %in% c("Biology", "Chemistry", "Physics", "Computers and Electronics", "Engineering and Technology", "Mathematics")) %>%
-  filter(Scale.ID == "IM")%>%
+  filter(Scale.ID == "LV")%>%
   transmute(O.NET.SOC.Code, Element.Name, Data.Value) -> stemKnowledgeLong
-#write.csv(stemKnowledgeLong, "stem knowledge importance.csv")
+write.csv(stemKnowledgeLong, "./data/stem_edu/working/stem knowledge level.csv")
 
 stemKnowledgeWide = cast(stemKnowledgeLong, O.NET.SOC.Code ~ Element.Name)
 rownames(stemKnowledgeWide) = stemKnowledgeWide[,1]
@@ -33,4 +38,6 @@ abline(v = centers[-3] + diff(centers)/2)
 
 allKnowledge = filter(knowledge, Scale.ID == "IM")
 
+# Incorporate knowledge crosswalk
 
+crosswalk = read.csv(paste0("./data/stem_edu/original/occupation_cw.csv"), header = T)[,-1]
