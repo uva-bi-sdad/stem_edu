1c496b155821366ebaaec639f00cda3856f5dfc5
Ian Crandell <ian85@vbi.vt.edu>
2018-03-09 17:05:57 +0000

    Added notes

---
+++ src/jobsCountTables/makeJOLTSJobTable.R
@@ -0,0 +1,3 @@
+# Conclusion: use fipscounty. 3271 fipscounties, 410 CBSAs. To compare to Datawork we aggregate JOLTS data to CBSAs.
+
+# Data aggregations needed: for JOLTS, Datawork, and Openjobs, a table with CBSA/County/Month-Yr/#jobs/#stem jobs
