932b0565300a1bc87e6e1ad59c203dbb1565ff36
Emily Sheen <sheen@vt.edu>
2018-06-21 14:59:58 +0000

    clean code, delete old ipeds exploration

--- src/data_discovery/merging_ipeds_data/merge_hd_directory_info.R
+++ src/data_discovery/merging_ipeds_data/merge_hd_directory_info.R
@@ -1,28 +1,10 @@
 #########################################################
-###          IPEDS Data - Exploratory Code            ###
+###          IPEDS Data - Code to Merge hd            ###
+###  Institution Characteristics & Directory Info     ###
 ###                   Emily Sheen                     ###
 #########################################################
 
-################## Old IPEDS Data #######################
 
-library("dplyr")
-# unzip("./data/stem_edu/original/IPEDS_data_zipped/IPEDS_2015_degrees.zip",
-#       exdir = "./data/stem_edu/original/DSPG2018DataDiscovery/IPEDS")
-#
-# pathfile <- "./data/stem_edu/original/DSPG2018DataDiscovery/IPEDS/CSV_7112017-625.csv"
-# degrees15 <- read.csv(pathfile)
-#
-# degrees15 <- as.data.frame(degrees15)
-# levels(degrees15$C2015_A.Award.Level.code)
-#
-# assoc <- degrees15 %>% filter(C2015_A.Award.Level.code =="Associate's degree")
-# View(assoc)
-#
-# levels(assoc$CipTitle)
-
-
-
-#####################  New IPEDS Data ###################
 library("dplyr")
 
 library(readr)
