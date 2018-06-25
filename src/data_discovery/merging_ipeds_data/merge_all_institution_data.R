b1ddcf931b55f032dee94e34062d820d08b31e68
Emily Sheen <sheen@vt.edu>
2018-06-25 18:43:35 +0000

    edit merge code

--- src/data_discovery/merging_ipeds_data/merge_all_institution_data.R
+++ src/data_discovery/merging_ipeds_data/merge_all_institution_data.R
@@ -64,39 +64,36 @@ output = dbWriteTable(conn = conn,
 # Realize that merging the tables is a pain, will keep separate tables to do analysis
 
 
+#
+# Run the combining_ipeds.sql script before running below code
+#
 
+DBI::dbGetQuery(conn, 'SELECT hd2012_2017.unitid, hd2012_2017.instnm, hd2012_2017.addr, hd2012_2017.city,
+hd2012_2017.stabbr, hd2012_2017.zip, hd2012_2017.fips, hd2012_2017.sector,
+                hd2012_2017.iclevel, hd2012_2017.hloffer, hd2012_2017.ugoffer,
+                hd2012_2017.groffer, hd2012_2017.hdegofr1, hd2012_2017.deggrant,
+                hd2012_2017.instcat, hd2012_2017.countycd, hd2012_2017.longitud,
+                hd2012_2017.latitude, ic2012_2017.level1, ic2012_2017.level2,
+                ic2012_2017.level3,
+                ic2012_2017.level4,
+                ic2012_2017.level5,
+                ic2012_2017.level6,
+                ic2012_2017.level7,
+                ic2012_2017.level8,
+                ic2012_2017.level12,
+                ic2012_2017.level17,
+                ic2012_2017.level18,
+                ic2012_2017.level19,
+                ic2012_2017.distcrs,
+                ic2012_2017.distnced,
+                ic2012_2017.dstnced1,
+                ic2012_2017.dstnced2,
+                ic2012_2017.dstnced3
 
-#Send SQL query to merge the 5 tables into one master table
-DBI::dbCreateTable(conn,
-  'SELECT hd2012_2017.unitid, hd2012_2017.instnm, hd2012_2017.addr, hd2012_2017.city,
-  hd2012_2017.stabbr, hd2012_2017.zip, hd2012_2017.fips, hd2012_2017.sector,
-                 hd2012_2017.iclevel, hd2012_2017.hloffer, hd2012_2017.ugoffer,
-                 hd2012_2017.groffer, hd2012_2017.hdegofr1, hd2012_2017.deggrant,
-                 hd2012_2017.instcat, hd2012_2017.countycd, hd2012_2017.longitud,
-                 hd2012_2017.latitude, ic2012_2017.level1, ic2012_2017.level2,
-                 ic2012_2017.level3,
-                 ic2012_2017.level4,
-                 ic2012_2017.level5,
-                 ic2012_2017.level6,
-                 ic2012_2017.level7,
-                 ic2012_2017.level8,
-                 ic2012_2017.level12,
-                 ic2012_2017.level17,
-                 ic2012_2017.level18,
-                 ic2012_2017.level19,
-                 ic2012_2017.distcrs,
-                 ic2012_2017.distnced,
-                 ic2012_2017.dstnced1,
-                 ic2012_2017.dstnced2,
-                 ic2012_2017.dstnced3
-                 FROM hd2012_2017
-                 FULL OUTER JOIN ic2012_2017
-                 ON hd2012_2017.unitid = ic2012_2017.unitid
-                 AND hd2012_2017.year = ic2012_2017.year')
-
-# use SQL to get what you want
-
-DBI::dbGetQuery(conn 'SELECT col1 col2 col3 from ipeds_master;')
+                FROM hd2012_2017
+                FULL OUTER JOIN ic2012_2017
+                ON hd2012_2017.unitid = ic2012_2017.unitid
+                AND hd2012_2017.year = ic2012_2017.year;')
 
 
 
