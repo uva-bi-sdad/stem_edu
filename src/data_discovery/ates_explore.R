57cfcbb0b315c497daa371bcd9df588a09ddff56
Emily Sheen <sheen@vt.edu>
2018-07-09 18:09:17 +0000

    minor additions

--- src/data_discovery/ates_explore.R
+++ src/data_discovery/ates_explore.R
@@ -7,3 +7,10 @@ load("./data/stem_edu/original/ATES/ates_pu_pert.rdata")
 
 ates <- ates_pu_pert
 View(ates)
+
+colnames(ates)
+levels(as.factor(ates$path))
+levels(as.factor(ates$CNINVALID1))
+length(which(ates$CNINVALID1 == 1))
+
+#Analysis of first (most important) certifications
