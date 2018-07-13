6fe5850e25287af0c615ac10bcffc18d3c7ce637
Emily Sheen <sheen@vt.edu>
2018-07-13 14:57:10 +0000

    add plots

--- src/data_discovery/ATES/ates_explore.R
+++ src/data_discovery/ATES/ates_explore.R
@@ -16,7 +16,6 @@ ates <- fread("./data/stem_edu/working/DSPG18/ATES/ates.csv")
 varnames <- fread("./data/stem_edu/working/DSPG18/ATES/varnames.csv")
 codenames <- fread("./data/stem_edu/working/DSPG18/ATES/codenames.csv")
 
-
 #Histogram for educational attainment of survey participants
 key_eduattn <- filter(codenames, varname == "EDUATTN")
 key_eduattn[1,3] <- "No high school diploma or GED"
@@ -41,5 +40,22 @@ ggplot(data = ates_key) +
   ylab("Count of Survey Respondents")
 
 
-#Analysis of first (most important) certifications
+
+#Histogram for field of study highest degree
+key_edufos <- filter(codenames, varname == "EDUFOS")
+class(key_edufos$varcode)
+class(ates$edufos)
+key_edufos$varcode <- as.integer(key_edufos$varcode)
+  #Merge data with key
+ates_key <- left_join(ates, select(key_edufos, varcode, code_desc), by = c("edufos" = "varcode"))
+head(ates_key)
+  #make code_desc wrap text for prettier plot
+ates_key$code_desc <- str_wrap(ates_key$code_desc, width = 50)
+  #Educational attainment histogram
+ggplot(data = ates_key) +
+  geom_bar(aes(code_desc)) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  xlab("Field of Study of Highest Degree") +
+  ylab("Count of Survey Respondents") +
+  ggtitle("Fields of Study of Survey Respondents")
 
