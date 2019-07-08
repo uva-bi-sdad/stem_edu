aba92c8f44c64c385aac01525415a149120563be
sm9dv <sm9dv@virginia.edu>
2019-07-08 20:58:37 +0000

    mapping education in MSAs and exploratory analysis

--- src/sarah/exploratory/describe_top5.R
+++ src/sarah/exploratory/describe_top5.R
@@ -6,7 +6,118 @@ b_tot <- read.csv("data/stem_edu/working/skills_by_occupation/top5_rb_separate/b
 r_tot <- read.csv("data/stem_edu/working/skills_by_occupation/top5_rb_separate/r_top5-stw-jobskill.csv")
 
 #Onetnames of top 5 for blacksburg and richmond
+b_onet <- unique(b_tot$onet)
+r_onet <- unique(r_tot$onet)
+
 b_onetnames <- unique(b_tot$onetname)
 r_onetnames <- unique(r_tot$onetname)
 
+###BLACKSBURG
+# number of unique skills, clusters, families associated with each job
+
+count_b <- b_tot %>%
+  select(bgtjobid, skill, skillcluster, skillclusterfamily) %>%
+  group_by(bgtjobid) %>%
+  summarize(num_s =n_distinct(skill), num_sc = n_distinct(skillcluster), num_scf=n_distinct(skillclusterfamily))
+
+
+
+#number of skills, cluster, family frequency
+
+freq_num_s_b <- count_b %>% group_by(num_s) %>% summarise(count = n())
+  freq_num_s_b$total = length(unique(b_tot$bgtjobid))
+  freq_num_s_b$per = ((freq_num_s_b$count)/(freq_num_s_b$total))*100
+  freq_num_s_b$variable = "Blacksburg"
+
+freq_num_sc_b <- count_b %>% group_by(num_sc) %>% summarise(count = n())
+  freq_num_sc_b$total = length(unique(b_tot$bgtjobid))
+  freq_num_sc_b$per = ((freq_num_sc_b$count)/(freq_num_sc_b$total))*100
+  freq_num_sc_b$variable = "Blacksburg"
+
+freq_num_scf_b <- count_b %>% group_by(num_scf) %>% summarise(count = n())
+  freq_num_scf_b$total = length(unique(b_tot$bgtjobid))
+  freq_num_scf_b$per = ((freq_num_scf_b$count)/(freq_num_scf_b$total))*100
+  freq_num_scf_b$variable = "Blacksburg"
+
+###RICHMOND
+# number of unique skills, clusters, families associated with each job
+count_r <- r_tot %>%
+  select(bgtjobid, skill, skillcluster, skillclusterfamily) %>%
+  group_by(bgtjobid) %>%
+  summarize(num_s =n_distinct(skill), num_sc = n_distinct(skillcluster), num_scf=n_distinct(skillclusterfamily))
+
+#number of skills, cluster, family frequency
+freq_num_s_r <- count_r %>% group_by(num_s) %>% summarise(count = n())
+  freq_num_s_r$total = length(unique(r_tot$bgtjobid))
+  freq_num_s_r$per = ((freq_num_s_r$count)/(freq_num_s_r$total))*100
+  freq_num_s_r$variable = "Richmond"
+
+freq_num_sc_r <- count_r %>% group_by(num_sc) %>% summarise(count = n())
+  freq_num_sc_r$total = length(unique(r_tot$bgtjobid))
+  freq_num_sc_r$per = ((freq_num_sc_r$count)/(freq_num_sc_r$total))*100
+  freq_num_sc_r$variable = "Richmond"
+
+freq_num_scf_r <- count_r %>% group_by(num_scf) %>% summarise(count = n())
+  freq_num_scf_r$total = length(unique(r_tot$bgtjobid))
+  freq_num_scf_r$per = ((freq_num_scf_r$count)/(freq_num_scf_r$total))*100
+  freq_num_scf_r$variable = "Richmond"
+
+#COMPARE FREQUENCIES FOR TOTAL BLACKSBURG AND RICHMOND
+freq_num_s_br <- rbind(freq_num_s_b, freq_num_s_r)
+  ggplot(freq_num_s_br, aes(num_s, per)) +
+    geom_bar(aes(fill = variable), position = "dodge", stat="identity")
+freq_num_sc_br <- rbind(freq_num_sc_b, freq_num_sc_r)
+  ggplot(freq_num_sc_br, aes(num_sc, per)) +
+    geom_bar(aes(fill = variable), position = "dodge", stat="identity")
+freq_num_scf_br <- rbind(freq_num_scf_b, freq_num_scf_r)
+  ggplot(freq_num_scf_br, aes(num_scf, per)) +
+    geom_bar(aes(fill = variable), position = "dodge", stat="identity")
+
+#ensuring the nurse onet codes are the same except for machinists and web developers
+b_onet<- select(b_tot, c("onet", "onetname"))
+r_onet<- select(r_tot, c("onet", "onetname"))
+  unique(b_tot$onet[!(b_tot$onet %in% r_tot$onet)])
+  unique(r_tot$onet[!(r_tot$onet %in% b_tot$onet)])
+
+#Blacksburg by ONETname
+b_maint <- filter(b_tot, onet == "49-9071.00")
+  count_b_maint <- b_maint %>%
+    select(bgtjobid, skill, skillcluster, skillclusterfamily) %>%
+    group_by(bgtjobid) %>%
+    summarize(num_s =n_distinct(skill), num_sc = n_distinct(skillcluster), num_scf=n_distinct(skillclusterfamily))
+b_nurs <- filter(b_tot, onet == "29-1141.03")
+  count_b_nurs <- b_nurs %>%
+    select(bgtjobid, skill, skillcluster, skillclusterfamily) %>%
+    group_by(bgtjobid) %>%
+    summarize(num_s =n_distinct(skill), num_sc = n_distinct(skillcluster), num_scf=n_distinct(skillclusterfamily))
+b_comp <- filter(b_tot, onet=="15-1151.00")
+  count_b_comp <- b_comp %>%
+    select(bgtjobid, skill, skillcluster, skillclusterfamily) %>%
+    group_by(bgtjobid) %>%
+    summarize(num_s =n_distinct(skill), num_sc = n_distinct(skillcluster), num_scf=n_distinct(skillclusterfamily))
+b_mach <- filter(b_tot, onet =="51-4041.00")
+  count_b_mach <- b_mach %>%
+    select(bgtjobid, skill, skillcluster, skillclusterfamily) %>%
+    group_by(bgtjobid) %>%
+    summarize(num_s =n_distinct(skill), num_sc = n_distinct(skillcluster), num_scf=n_distinct(skillclusterfamily))
+b_auto <- filter(b_tot, onet == "49-3023.02")
+  count_b_auto <- b_auto %>%
+    select(bgtjobid, skill, skillcluster, skillclusterfamily) %>%
+    group_by(bgtjobid) %>%
+    summarize(num_s =n_distinct(skill), num_sc = n_distinct(skillcluster), num_scf=n_distinct(skillclusterfamily))
+
+#Richmond by ONETname
+r_maint <- filter(r_tot, onet == "49-9071.00")
+  count_r_maint <- r_maint %>%
+    select(bgtjobid, skill, skillcluster, skillclusterfamily) %>%
+    group_by(bgtjobid) %>%
+    summarize(num_s =n_distinct(skill), num_sc = n_distinct(skillcluster), num_scf=n_distinct(skillclusterfamily))
+r_auto <- filter(r_tot, onet == "49-3023.02")
+  count_r_auto <- r_auto %>%
+    select(bgtjobid, skill, skillcluster, skillclusterfamily) %>%
+    group_by(bgtjobid) %>%
+    summarize(num_s =n_distinct(skill), num_sc = n_distinct(skillcluster), num_scf=n_distinct(skillclusterfamily))
+r_comp <- filter(r_tot, onet=="15-1151.00")
+r_nurs <- filter(r_tot, onet == "29-1141.03")
+r_web <- filter(r_tot, onet == "15-1134.00")
 
--- src/sarah/msa_schev_vccs.R
+++ src/sarah/msa_schev_vccs.R
@@ -1,5 +1,7 @@
 library(sf)
 library(tidyverse)
+library(tidyr)
+library(reshape2)
 
 #load MSA data from Census TIGER/Line Shapefiles
 msa <- st_read("src/sarah/tl_2017_us_cbsa/tl_2017_us_cbsa.shp")
@@ -9,15 +11,17 @@ st_crs(msa) #unprojected crs
 #filter for richmond and blacksburg MSAs
 r_msa <- filter(msa, NAME == "Richmond, VA")
 b_msa <- filter(msa, NAME == "Blacksburg-Christiansburg-Radford, VA")
+msa <- rbind(r_msa, b_msa)
 
 #load VCCS data
 vccs <- read.csv("data/stem_edu/working/ipeds_vccs/ipeds_vccs_cred.csv")
+names(vccs)[names(vccs) == "College"] <- "institution"
 
-levels(vccs$College)[levels(vccs$College)=="J. Sargeant Reynolds Community College"] <- "J Sargeant Reynolds Community College"
+levels(vccs$institution)[levels(vccs$institution)=="J. Sargeant Reynolds Community College"] <- "J Sargeant Reynolds Community College"
 
 
 #filter to unique College, long and lat
-vccs <- ipeds_vccs_cred[c(2, 13:14)]
+vccs <- vccs[c(2, 13:14)]
 vccs <- unique(vccs)
 head(vccs)
 
@@ -27,9 +31,11 @@ vccs <- vccs %>%
   st_set_crs(value = st_crs(x = msa))
 st_crs(vccs)
 
+
+
 #shows which VCCS colleges are in richmond, then blacksburg
-r_msa_vccs <- st_join(x = r_msa, y = vccs)
-b_msa_vccs <- st_join(x = b_msa, y = vccs)
+#r_msa_vccs <- st_join(x = r_msa, y = vccs)
+#b_msa_vccs <- st_join(x = b_msa, y = vccs)
 
 #load SCHEV STW Schools data
 stw_schools <- read.csv("data/stem_edu/working/ipeds_schev/ipeds_schev_stw_schools.csv")
@@ -45,9 +51,10 @@ stw_schools <- stw_schools %>%
   st_set_crs(value = st_crs(x = msa))
 st_crs(stw_schools)
 
+
 #shows which SCHEV STW SCHOOLS  are in richmond, then blacksburg
-r_msa_stw_schools <- st_join(x = r_msa, y = stw_schools)
-b_msa_stw_schools <- st_join(x = b_msa, y = stw_schools)
+#r_msa_stw_schools <- st_join(x = r_msa, y = stw_schools)
+#b_msa_stw_schools <- st_join(x = b_msa, y = stw_schools)
 
 
 #load SCHEV STW subbach combine data
@@ -64,9 +71,11 @@ stw_subbach_combine <- stw_subbach_combine %>%
   st_set_crs(value = st_crs(x = msa))
 st_crs(stw_subbach_combine)
 
+
+
 #shows which SCHEV STW SUBBACH COMBINE  are in richmond, then blacksburg
-r_msa_stw_subbach_combine <- st_join(x = r_msa, y = stw_subbach_combine)
-b_msa_stw_subbach_combine <- st_join(x = b_msa, y = stw_subbach_combine)
+#r_msa_stw_subbach_combine <- st_join(x = r_msa, y = stw_subbach_combine)
+#b_msa_stw_subbach_combine <- st_join(x = b_msa, y = stw_subbach_combine)
 
 #load SCHEV  subbach combine data
 subbach_combine <- read.csv("data/stem_edu/working/ipeds_schev/ipeds_schev_subbach_combine.csv")
@@ -82,50 +91,98 @@ subbach_combine <- subbach_combine %>%
   st_set_crs(value = st_crs(x = msa))
 st_crs(subbach_combine)
 
+
 #shows which SCHEV  SUBBACH COMBINE  are in richmond, then blacksburg
-r_msa_subbach_combine <- st_join(x = r_msa, y = subbach_combine)
-b_msa_subbach_combine <- st_join(x = b_msa, y = subbach_combine)
+#r_msa_subbach_combine <- st_join(x = r_msa, y = subbach_combine)
+#b_msa_subbach_combine <- st_join(x = b_msa, y = subbach_combine)
 
 
+msa_vccs <- st_join(x=msa, y=vccs)
+msa_stw_schools <- st_join(x=msa, y=stw_schools)
+msa_stw_subbach_combine <- st_join(x=msa, y=stw_subbach_combine)
+msa_subbach_combine <- st_join(x=msa, y=subbach_combine)
 
+msa_edu <- rbind(msa_vccs, msa_stw_schools, msa_stw_subbach_combine, msa_subbach_combine)
+msa_edu <- unique(msa_edu)
+msa_edu <- msa_edu %>% select(institution, geometry)
+
+vccs$type = "VCCS"
+stw_schools$type = "stw_schools"
+stw_subbach_combine$type = "stw_subbach_combine"
+subbach_combine$type = "subbach_combine"
 
 
 ###COMBINE ALL
-names(b_msa_vccs)[names(b_msa_vccs) == "College"] <- "institution"
-names(r_msa_vccs)[names(r_msa_vccs) == "College"] <- "institution"
+edu <- rbind(stw_schools, subbach_combine, stw_subbach_combine, vccs)
+edu$value =TRUE
+edu <- spread(edu, type, value)
+library(data.table)
+edu <- setDT(edu)[institution %chin% msa_edu$institution]
 
-b <- rbind(b_msa_stw_schools, b_msa_stw_subbach_combine, b_msa_subbach_combine)
-b <- unique(b)
+colnames(edu)[colnames(edu)=="institution"] <- "object"
+edu <- replace(edu, is.na(edu), FALSE)
 
-colnames(b)
-colnames(b_msa_vccs)
+msa <- msa %>% select(NAME, geometry)
+colnames(msa)[colnames(msa)=="NAME"] <- "object"
 
-b_msa_vccs <- b_msa_vccs[ -c(13, 15:29) ]
+msa$stw_schools = NA
+msa$stw_subbach_combine = NA
+msa$subbach_combine = NA
+msa$VCCS = NA
+msa <- msa[,c(1,3,4,5,6,2)]
 
-colnames(b)
-colnames(b_msa_vccs)
+p <-ggplot()+
+  geom_sf(data=st_va)+
+  geom_sf(data = msa, fill = NA) +
+  geom_sf(data = edu)
 
-b<- rbind(b, b_msa_vccs)
-b <- unique(b)
-head(b)
 
-r<- rbind(r_msa_stw_schools, r_msa_stw_subbach_combine, r_msa_subbach_combine)
-r <- unique(r)
+st <- st_read("src/sarah/tl_2017_us_state/tl_2017_us_state.shp")
+st_va <- filter(st, STUSPS == "VA")
 
-colnames (r)
-colnames(r_msa_vccs)
 
-r_msa_vccs <- r_msa_vccs[ -c(13, 15:29) ]
-colnames (r)
-colnames(r_msa_vccs)
 
-r <- rbind(r, r_msa_vccs)
-r <- unique(r)
-head(r)
 
-library(ggplot2)
-ggplot(r_geo)+
-  geom_sf()
 
-ggplot(vccs_geo)+
-  geom_sf()
+
+
+
+#br <- rbind(b, r)
+
+
+
+
+
+#b <- rbind(b_msa_stw_schools, b_msa_stw_subbach_combine, b_msa_subbach_combine)
+#b <- unique(b)
+
+#colnames(b)
+#colnames(b_msa_vccs)
+
+#b_msa_vccs <- b_msa_vccs[ -c(13, 15:29) ]
+
+#colnames(b)
+#colnames(b_msa_vccs)
+
+#b<- rbind(b, b_msa_vccs)
+#b <- unique(b)
+#head(b)
+
+#r<- rbind(r_msa_stw_schools, r_msa_stw_subbach_combine, r_msa_subbach_combine)
+#r <- unique(r)
+
+#colnames (r)
+#colnames(r_msa_vccs)
+
+#r_msa_vccs <- r_msa_vccs[ -c(13, 15:29) ]
+#colnames (r)
+#colnames(r_msa_vccs)
+
+#r <- rbind(r, r_msa_vccs)
+#r <- unique(r)
+#head(r)
+
+
+
+
+
