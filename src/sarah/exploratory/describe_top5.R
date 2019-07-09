c9fc03d91c5fa221da5305395b75de3ce5709635
sm9dv <sm9dv@virginia.edu>
2019-07-09 20:57:55 +0000

    exploratory analysis and mapping education

--- src/sarah/exploratory/describe_top5.R
+++ src/sarah/exploratory/describe_top5.R
@@ -9,21 +9,15 @@ r_tot <- read.csv("data/stem_edu/working/skills_by_occupation/top5_rb_separate/r
 b_onet <- unique(b_tot$onet)
 r_onet <- unique(r_tot$onet)
 
-b_onetnames <- unique(b_tot$onetname)
-r_onetnames <- unique(r_tot$onetname)
-
 ###BLACKSBURG
-# number of unique skills, clusters, families associated with each job
 
+# number of unique skills, clusters, families associated with each job
 count_b <- b_tot %>%
-  select(bgtjobid, skill, skillcluster, skillclusterfamily) %>%
-  group_by(bgtjobid) %>%
+  select(bgtjobid, skill, skillcluster, skillclusterfamily, onet) %>%
+  group_by(bgtjobid, onet) %>%
   summarize(num_s =n_distinct(skill), num_sc = n_distinct(skillcluster), num_scf=n_distinct(skillclusterfamily))
 
-
-
 #number of skills, cluster, family frequency
-
 freq_num_s_b <- count_b %>% group_by(num_s) %>% summarise(count = n())
   freq_num_s_b$total = length(unique(b_tot$bgtjobid))
   freq_num_s_b$per = ((freq_num_s_b$count)/(freq_num_s_b$total))*100
@@ -42,8 +36,8 @@ freq_num_scf_b <- count_b %>% group_by(num_scf) %>% summarise(count = n())
 ###RICHMOND
 # number of unique skills, clusters, families associated with each job
 count_r <- r_tot %>%
-  select(bgtjobid, skill, skillcluster, skillclusterfamily) %>%
-  group_by(bgtjobid) %>%
+  select(bgtjobid, skill, skillcluster, skillclusterfamily, onet) %>%
+  group_by(bgtjobid, onet) %>%
   summarize(num_s =n_distinct(skill), num_sc = n_distinct(skillcluster), num_scf=n_distinct(skillclusterfamily))
 
 #number of skills, cluster, family frequency
@@ -65,13 +59,21 @@ freq_num_scf_r <- count_r %>% group_by(num_scf) %>% summarise(count = n())
 #COMPARE FREQUENCIES FOR TOTAL BLACKSBURG AND RICHMOND
 freq_num_s_br <- rbind(freq_num_s_b, freq_num_s_r)
   ggplot(freq_num_s_br, aes(num_s, per)) +
-    geom_bar(aes(fill = variable), position = "dodge", stat="identity")
+    geom_bar(aes(fill = variable), position = "dodge", stat="identity") +
+    ggtitle("Percent of Job Ads Requesting Number of Skills for \nTop Five STW Occupations in Richmond and Blacksburg") +
+    labs(y="% Percent of Job Ads", x ="Number of Skills Requested")
+
 freq_num_sc_br <- rbind(freq_num_sc_b, freq_num_sc_r)
   ggplot(freq_num_sc_br, aes(num_sc, per)) +
-    geom_bar(aes(fill = variable), position = "dodge", stat="identity")
+    geom_bar(aes(fill = variable), position = "dodge", stat="identity")+
+    ggtitle("Percent of Job Ads Requesting Number of Skill Clusters for \nTop Five STW Occupations in Richmond and Blacksburg") +
+    labs(y="% Percent of Job Ads", x ="Number of Skill Clusters Requested")
+
 freq_num_scf_br <- rbind(freq_num_scf_b, freq_num_scf_r)
   ggplot(freq_num_scf_br, aes(num_scf, per)) +
-    geom_bar(aes(fill = variable), position = "dodge", stat="identity")
+    geom_bar(aes(fill = variable), position = "dodge", stat="identity")+
+    ggtitle("Percent of Job Ads Requesting Number of Skill Cluster Families for \nTop Five STW Occupations in Richmond and Blacksburg") +
+    labs(y="% Percent of Job Ads", x ="Number of Skill Cluster Families Requested")
 
 #ensuring the nurse onet codes are the same except for machinists and web developers
 b_onet<- select(b_tot, c("onet", "onetname"))
@@ -80,44 +82,252 @@ r_onet<- select(r_tot, c("onet", "onetname"))
   unique(r_tot$onet[!(r_tot$onet %in% b_tot$onet)])
 
 #Blacksburg by ONETname
-b_maint <- filter(b_tot, onet == "49-9071.00")
-  count_b_maint <- b_maint %>%
-    select(bgtjobid, skill, skillcluster, skillclusterfamily) %>%
-    group_by(bgtjobid) %>%
-    summarize(num_s =n_distinct(skill), num_sc = n_distinct(skillcluster), num_scf=n_distinct(skillclusterfamily))
-b_nurs <- filter(b_tot, onet == "29-1141.03")
-  count_b_nurs <- b_nurs %>%
-    select(bgtjobid, skill, skillcluster, skillclusterfamily) %>%
-    group_by(bgtjobid) %>%
-    summarize(num_s =n_distinct(skill), num_sc = n_distinct(skillcluster), num_scf=n_distinct(skillclusterfamily))
-b_comp <- filter(b_tot, onet=="15-1151.00")
-  count_b_comp <- b_comp %>%
-    select(bgtjobid, skill, skillcluster, skillclusterfamily) %>%
-    group_by(bgtjobid) %>%
-    summarize(num_s =n_distinct(skill), num_sc = n_distinct(skillcluster), num_scf=n_distinct(skillclusterfamily))
-b_mach <- filter(b_tot, onet =="51-4041.00")
-  count_b_mach <- b_mach %>%
-    select(bgtjobid, skill, skillcluster, skillclusterfamily) %>%
-    group_by(bgtjobid) %>%
-    summarize(num_s =n_distinct(skill), num_sc = n_distinct(skillcluster), num_scf=n_distinct(skillclusterfamily))
-b_auto <- filter(b_tot, onet == "49-3023.02")
-  count_b_auto <- b_auto %>%
-    select(bgtjobid, skill, skillcluster, skillclusterfamily) %>%
-    group_by(bgtjobid) %>%
-    summarize(num_s =n_distinct(skill), num_sc = n_distinct(skillcluster), num_scf=n_distinct(skillclusterfamily))
+count_b_maint <- filter(count_b, onet=="49-9071.00")
+  freq_s_b_maint <- count_b_maint %>% group_by(num_s) %>% summarise(count = n())
+    freq_s_b_maint$total = length(unique(count_b_maint$bgtjobid))
+    freq_s_b_maint$per = ((freq_s_b_maint$count)/(freq_s_b_maint$total))*100
+    freq_s_b_maint$variable = "b_maint"
+  freq_sc_b_maint <- count_b_maint %>% group_by(num_sc) %>% summarise(count = n())
+    freq_sc_b_maint$total = length(unique(count_b_maint$bgtjobid))
+    freq_sc_b_maint$per = ((freq_sc_b_maint$count)/(freq_sc_b_maint$total))*100
+    freq_sc_b_maint$variable = "b_maint"
+  freq_scf_b_maint <- count_b_maint %>% group_by(num_scf) %>% summarise(count = n())
+    freq_scf_b_maint$total = length(unique(count_b_maint$bgtjobid))
+    freq_scf_b_maint$per = ((freq_scf_b_maint$count)/(freq_scf_b_maint$total))*100
+    freq_scf_b_maint$variable = "b_maint"
+
+count_b_nurs <- filter(count_b, onet == "29-1141.03")
+  freq_s_b_nurs <- count_b_nurs %>% group_by(num_s) %>% summarise(count = n())
+    freq_s_b_nurs$total = length(unique(count_b_nurs$bgtjobid))
+    freq_s_b_nurs$per = ((freq_s_b_nurs$count)/(freq_s_b_nurs$total))*100
+    freq_s_b_nurs$variable = "b_nurs"
+  freq_sc_b_nurs <- count_b_nurs %>% group_by(num_sc) %>% summarise(count = n())
+    freq_sc_b_nurs$total = length(unique(count_b_nurs$bgtjobid))
+    freq_sc_b_nurs$per = ((freq_sc_b_nurs$count)/(freq_sc_b_nurs$total))*100
+    freq_sc_b_nurs$variable = "b_nurs"
+  freq_scf_b_nurs <- count_b_nurs %>% group_by(num_scf) %>% summarise(count = n())
+    freq_scf_b_nurs$total = length(unique(count_b_nurs$bgtjobid))
+    freq_scf_b_nurs$per = ((freq_scf_b_nurs$count)/(freq_scf_b_nurs$total))*100
+    freq_scf_b_nurs$variable = "b_nurs"
+
+count_b_comp <- filter(count_b, onet=="15-1151.00")
+  freq_s_b_comp <- count_b_comp %>% group_by(num_s) %>% summarise(count = n())
+    freq_s_b_comp$total = length(unique(count_b_comp$bgtjobid))
+    freq_s_b_comp$per = ((freq_s_b_comp$count)/(freq_s_b_comp$total))*100
+    freq_s_b_comp$variable = "b_comp"
+  freq_sc_b_comp <- count_b_comp %>% group_by(num_sc) %>% summarise(count = n())
+    freq_sc_b_comp$total = length(unique(count_b_comp$bgtjobid))
+    freq_sc_b_comp$per = ((freq_sc_b_comp$count)/(freq_sc_b_comp$total))*100
+    freq_sc_b_comp$variable = "b_comp"
+  freq_scf_b_comp <- count_b_comp %>% group_by(num_scf) %>% summarise(count = n())
+    freq_scf_b_comp$total = length(unique(count_b_comp$bgtjobid))
+    freq_scf_b_comp$per = ((freq_scf_b_comp$count)/(freq_scf_b_comp$total))*100
+    freq_scf_b_comp$variable = "b_comp"
+count_b_mach <- filter(count_b, onet =="51-4041.00")
+  freq_s_b_mach <- count_b_mach %>% group_by(num_s) %>% summarise(count = n())
+    freq_s_b_mach$total = length(unique(count_b_mach$bgtjobid))
+    freq_s_b_mach$per = ((freq_s_b_mach$count)/(freq_s_b_mach$total))*100
+    freq_s_b_mach$variable = "b_mach"
+  freq_sc_b_mach <- count_b_mach %>% group_by(num_sc) %>% summarise(count = n())
+    freq_sc_b_mach$total = length(unique(count_b_mach$bgtjobid))
+    freq_sc_b_mach$per = ((freq_sc_b_mach$count)/(freq_sc_b_mach$total))*100
+    freq_sc_b_mach$variable = "b_mach"
+  freq_scf_b_mach <- count_b_mach %>% group_by(num_scf) %>% summarise(count = n())
+    freq_scf_b_mach$total = length(unique(count_b_mach$bgtjobid))
+    freq_scf_b_mach$per = ((freq_scf_b_mach$count)/(freq_scf_b_mach$total))*100
+    freq_scf_b_mach$variable = "b_mach"
+count_b_auto <- filter(count_b, onet == "49-3023.02")
+  freq_s_b_auto <- count_b_auto %>% group_by(num_s) %>% summarise(count = n())
+    freq_s_b_auto$total = length(unique(count_b_auto$bgtjobid))
+    freq_s_b_auto$per = ((freq_s_b_auto$count)/(freq_s_b_auto$total))*100
+    freq_s_b_auto$variable = "b_auto"
+  freq_sc_b_auto <- count_b_auto %>% group_by(num_sc) %>% summarise(count = n())
+    freq_sc_b_auto$total = length(unique(count_b_auto$bgtjobid))
+    freq_sc_b_auto$per = ((freq_sc_b_auto$count)/(freq_sc_b_auto$total))*100
+    freq_sc_b_auto$variable = "b_auto"
+  freq_scf_b_auto <- count_b_auto %>% group_by(num_scf) %>% summarise(count = n())
+    freq_scf_b_auto$total = length(unique(count_b_auto$bgtjobid))
+    freq_scf_b_auto$per = ((freq_scf_b_auto$count)/(freq_scf_b_auto$total))*100
+    freq_scf_b_auto$variable = "b_auto"
+#CHART TOP 5 OCCUPATIONS OF BLACKSBURG BY SKILL, CLUSTER, FAMILIES
+freq_s_b_onet <- rbind(freq_s_b_auto, freq_s_b_comp, freq_s_b_mach, freq_s_b_maint, freq_s_b_nurs)
+  ggplot(freq_s_b_onet, aes(num_s, per)) +
+    geom_bar(aes(fill = variable), position = "dodge", stat="identity") +
+    ggtitle("Percent of Job Ads Requesting Number of Skills for \nTop Five STW Occupations in   Blacksburg") +
+    labs(y="% Percent of Job Ads", x ="Number of Skills Requested")
+
+freq_sc_b_onet <- rbind(freq_sc_b_auto, freq_sc_b_comp, freq_sc_b_mach, freq_sc_b_maint, freq_sc_b_nurs)
+  ggplot(freq_sc_b_onet, aes(num_sc, per)) +
+    geom_bar(aes(fill = variable), position = "dodge", stat="identity") +
+    ggtitle("Percent of Job Ads Requesting Number of Skill Clusters for \nTop Five STW Occupations in Blacksburg") +
+    labs(y="% Percent of Job Ads", x ="Number of Skill Clusters Requested")
+
+freq_scf_b_onet <- rbind(freq_scf_b_auto, freq_scf_b_comp, freq_scf_b_mach, freq_scf_b_maint, freq_scf_b_nurs)
+  ggplot(freq_scf_b_onet, aes(num_scf, per)) +
+    geom_bar(aes(fill = variable), position = "dodge", stat="identity") +
+    ggtitle("Percent of Job Ads Requesting Number of Skill Cluster Families for \nTop Five STW Occupations in Blacksburg") +
+    labs(y="% Percent of Job Ads", x ="Number of Skill Cluster Families Requested")
 
 #Richmond by ONETname
-r_maint <- filter(r_tot, onet == "49-9071.00")
-  count_r_maint <- r_maint %>%
-    select(bgtjobid, skill, skillcluster, skillclusterfamily) %>%
-    group_by(bgtjobid) %>%
-    summarize(num_s =n_distinct(skill), num_sc = n_distinct(skillcluster), num_scf=n_distinct(skillclusterfamily))
-r_auto <- filter(r_tot, onet == "49-3023.02")
-  count_r_auto <- r_auto %>%
-    select(bgtjobid, skill, skillcluster, skillclusterfamily) %>%
-    group_by(bgtjobid) %>%
-    summarize(num_s =n_distinct(skill), num_sc = n_distinct(skillcluster), num_scf=n_distinct(skillclusterfamily))
-r_comp <- filter(r_tot, onet=="15-1151.00")
-r_nurs <- filter(r_tot, onet == "29-1141.03")
-r_web <- filter(r_tot, onet == "15-1134.00")
+count_r_maint <- filter(count_r, onet=="49-9071.00")
+  freq_s_r_maint <- count_r_maint %>% group_by(num_s) %>% summarise(count = n())
+    freq_s_r_maint$total = length(unique(count_r_maint$bgtjobid))
+    freq_s_r_maint$per = ((freq_s_r_maint$count)/(freq_s_r_maint$total))*100
+    freq_s_r_maint$variable = "r_maint"
+  freq_sc_r_maint <- count_r_maint %>% group_by(num_sc) %>% summarise(count = n())
+    freq_sc_r_maint$total = length(unique(count_r_maint$bgtjobid))
+    freq_sc_r_maint$per = ((freq_sc_r_maint$count)/(freq_sc_r_maint$total))*100
+    freq_sc_r_maint$variable = "r_maint"
+  freq_scf_r_maint <- count_r_maint %>% group_by(num_scf) %>% summarise(count = n())
+    freq_scf_r_maint$total = length(unique(count_r_maint$bgtjobid))
+    freq_scf_r_maint$per = ((freq_scf_r_maint$count)/(freq_scf_r_maint$total))*100
+    freq_scf_r_maint$variable = "r_maint"
+count_r_nurs <- filter(count_r, onet == "29-1141.03")
+  freq_s_r_nurs <- count_r_nurs %>% group_by(num_s) %>% summarise(count = n())
+    freq_s_r_nurs$total = length(unique(count_r_nurs$bgtjobid))
+    freq_s_r_nurs$per = ((freq_s_r_nurs$count)/(freq_s_r_nurs$total))*100
+    freq_s_r_nurs$variable = "r_nurs"
+  freq_sc_r_nurs <- count_r_nurs %>% group_by(num_sc) %>% summarise(count = n())
+    freq_sc_r_nurs$total = length(unique(count_r_nurs$bgtjobid))
+    freq_sc_r_nurs$per = ((freq_sc_r_nurs$count)/(freq_sc_r_nurs$total))*100
+    freq_sc_r_nurs$variable = "r_nurs"
+  freq_scf_r_nurs <- count_r_nurs %>% group_by(num_scf) %>% summarise(count = n())
+    freq_scf_r_nurs$total = length(unique(count_r_nurs$bgtjobid))
+    freq_scf_r_nurs$per = ((freq_scf_r_nurs$count)/(freq_scf_r_nurs$total))*100
+    freq_scf_r_nurs$variable = "r_nurs"
+count_r_comp <- filter(count_r, onet=="15-1151.00")
+  freq_s_r_comp <- count_r_comp %>% group_by(num_s) %>% summarise(count = n())
+    freq_s_r_comp$total = length(unique(count_r_comp$bgtjobid))
+    freq_s_r_comp$per = ((freq_s_r_comp$count)/(freq_s_r_comp$total))*100
+    freq_s_r_comp$variable = "r_comp"
+  freq_sc_r_comp <- count_r_comp %>% group_by(num_sc) %>% summarise(count = n())
+    freq_sc_r_comp$total = length(unique(count_r_comp$bgtjobid))
+    freq_sc_r_comp$per = ((freq_sc_r_comp$count)/(freq_sc_r_comp$total))*100
+    freq_sc_r_comp$variable = "r_comp"
+  freq_scf_r_comp <- count_r_comp %>% group_by(num_scf) %>% summarise(count = n())
+    freq_scf_r_comp$total = length(unique(count_r_comp$bgtjobid))
+    freq_scf_r_comp$per = ((freq_scf_r_comp$count)/(freq_scf_r_comp$total))*100
+    freq_scf_r_comp$variable = "r_comp"
+count_r_auto <- filter(count_r, onet == "49-3023.02")
+  freq_s_r_auto <- count_r_auto %>% group_by(num_s) %>% summarise(count = n())
+    freq_s_r_auto$total = length(unique(count_r_auto$bgtjobid))
+    freq_s_r_auto$per = ((freq_s_r_auto$count)/(freq_s_r_auto$total))*100
+    freq_s_r_auto$variable = "r_auto"
+  freq_sc_r_auto <- count_r_auto %>% group_by(num_sc) %>% summarise(count = n())
+    freq_sc_r_auto$total = length(unique(count_r_auto$bgtjobid))
+    freq_sc_r_auto$per = ((freq_sc_r_auto$count)/(freq_sc_r_auto$total))*100
+    freq_sc_r_auto$variable = "r_auto"
+  freq_scf_r_auto <- count_r_auto %>% group_by(num_scf) %>% summarise(count = n())
+    freq_scf_r_auto$total = length(unique(count_r_auto$bgtjobid))
+    freq_scf_r_auto$per = ((freq_scf_r_auto$count)/(freq_scf_r_auto$total))*100
+    freq_scf_r_auto$variable = "r_auto"
+count_r_web <- filter(count_r, onet =="15-1134.00")
+  freq_s_r_web <- count_r_web %>% group_by(num_s) %>% summarise(count = n())
+    freq_s_r_web$total = length(unique(count_r_web$bgtjobid))
+    freq_s_r_web$per = ((freq_s_r_web$count)/(freq_s_r_web$total))*100
+    freq_s_r_web$variable = "r_web"
+  freq_sc_r_web <- count_r_web %>% group_by(num_sc) %>% summarise(count = n())
+    freq_sc_r_web$total = length(unique(count_r_web$bgtjobid))
+    freq_sc_r_web$per = ((freq_sc_r_web$count)/(freq_sc_r_web$total))*100
+    freq_sc_r_web$variable = "r_web"
+  freq_scf_r_web <- count_r_web %>% group_by(num_scf) %>% summarise(count = n())
+    freq_scf_r_web$total = length(unique(count_r_web$bgtjobid))
+    freq_scf_r_web$per = ((freq_scf_r_web$count)/(freq_scf_r_web$total))*100
+    freq_scf_r_web$variable = "r_web"
 
+#CHART TOP 5 OCCUPATIONS OF RICHMOND BY SKILL, CLUSTER, FAMILIES
+freq_s_r_onet <- rbind(freq_s_r_auto, freq_s_r_comp, freq_s_r_web, freq_s_r_maint, freq_s_r_nurs)
+  ggplot(freq_s_r_onet, aes(num_s, per)) +
+    geom_bar(aes(fill = variable), position = "dodge", stat="identity") +
+    ggtitle("Percent of Job Ads Requesting Number of Skills for \nTop Five STW Occupations in Richmond") +
+    labs(y="% Percent of Job Ads", x ="Number of Skills Requested")
+
+freq_sc_r_onet <- rbind(freq_sc_r_auto, freq_sc_r_comp, freq_sc_r_web, freq_sc_r_maint, freq_sc_r_nurs)
+  ggplot(freq_sc_r_onet, aes(num_sc, per)) +
+    geom_bar(aes(fill = variable), position = "dodge", stat="identity") +
+    ggtitle("Percent of Job Ads Requesting Number of Skill Clusters for \nTop Five STW Occupations in Richmond") +
+    labs(y="% Percent of Job Ads", x ="Number of Skill Clusters Requested")
+
+freq_scf_r_onet <- rbind(freq_scf_r_auto, freq_scf_r_comp, freq_scf_r_web, freq_scf_r_maint, freq_scf_r_nurs)
+  ggplot(freq_scf_r_onet, aes(num_scf, per)) +
+    geom_bar(aes(fill = variable), position = "dodge", stat="identity") +
+    ggtitle("Percent of Job Ads Requesting Number of Skill Cluster Families for \nTop Five STW Occupations in Richmond") +
+    labs(y="% Percent of Job Ads", x ="Number of Skill Cluster Families Requested")
+  freq_sc_r_onet <- rbind(freq_sc_r_auto, freq_sc_r_comp, freq_sc_r_web, freq_sc_r_maint, freq_sc_r_nurs)
+  ggplot(freq_sc_r_onet, aes(num_sc, per)) +
+    geom_bar(aes(fill = variable), position = "dodge", stat="identity") +
+    ggtitle("Percent of Job Ads Requesting Number of Skill Clusters for \nTop Five STW Occupations in Richmond") +
+    labs(y="% Percent of Job Ads", x ="Number of Skill Clusters Requested")
+
+  freq_scf_r_onet <- rbind(freq_scf_r_auto, freq_scf_r_comp, freq_scf_r_web, freq_scf_r_maint, freq_scf_r_nurs)
+  ggplot(freq_scf_r_onet, aes(num_scf, per)) +
+    geom_bar(aes(fill = variable), position = "dodge", stat="identity") +
+    ggtitle("Percent of Job Ads Requesting Number of Skill Cluster Families for \nTop Five STW Occupations in Richmond") +
+    labs(y="% Percent of Job Ads", x ="Number of Skill Cluster Families Requested")
+
+
+#COMPARE NURSES
+freq_s_nurs <- rbind(freq_s_b_nurs, freq_s_r_nurs)
+  ggplot(freq_s_nurs, aes(num_s, per)) +
+    geom_bar(aes(fill = variable), position = "dodge", stat="identity") +
+    ggtitle("Percent of Job Ads Requesting Number of Skills for \nCritical Care Nurses in Blacksburg and Richmond") +
+    labs(y="% Percent of Job Ads", x ="Number of Skills Requested")
+freq_sc_nurs <- rbind(freq_sc_b_nurs, freq_sc_r_nurs)
+  ggplot(freq_sc_nurs, aes(num_sc, per)) +
+    geom_bar(aes(fill = variable), position = "dodge", stat="identity") +
+    ggtitle("Percent of Job Ads Requesting Number of Skill Clusters for \nCritical Care Nurses in Blacksburg and Richmond") +
+    labs(y="% Percent of Job Ads", x ="Number of Skill Clusters Requested")
+freq_scf_nurs <- rbind(freq_scf_b_nurs, freq_scf_r_nurs)
+  ggplot(freq_scf_nurs, aes(num_scf, per)) +
+    geom_bar(aes(fill = variable), position = "dodge", stat="identity") +
+    ggtitle("Percent of Job Ads Requesting Number of Skill Cluster Families for \nCritical Care Nurses in Blacksburg and Richmond") +
+    labs(y="% Percent of Job Ads", x ="Number of Skill Cluster Families Requested")
+
+
+
+
+
+
+
+
+
+###ADDITIONAL METHOD
+  test <- r_tot %>% select(skillclusterfamily, onetname)
+  head(test)
+  test$count= 1
+  head(test)
+  test <-  aggregate(test$count, by= list(test$skillclusterfamily, test$onetname), FUN= sum)
+  head(test)
+
+  library(ggpubr)
+  ggballoonplot(test, fill = "value")+
+    scale_fill_viridis_c(option = "C")
+
+  test2 <- b_tot %>% select(skillclusterfamily, onetname)
+  head(test2)
+  test2$count= 1
+  head(test2)
+  test2 <-  aggregate(test2$count, by= list(test2$skillclusterfamily, test2$onetname), FUN= sum)
+  head(test2)
+
+  library(ggpubr)
+  ggballoonplot(test2, fill = "value")+
+    scale_fill_viridis_c(option = "C")
+
+
+
+
+
+
+
+  #colnames(freq_num_s_r)[colnames(freq_num_s_r)=="num_s"] <- "range"
+#colnames(freq_num_s_r)[colnames(freq_num_s_r)=="count"] <- "freq_num_s"
+#colnames(freq_num_s_r)[colnames(freq_num_s_r)=="per"] <- "per_num_s"
+
+#colnames(freq_num_sc_r)[colnames(freq_num_sc_r)=="num_sc"] <- "range"
+#colnames(freq_num_sc_r)[colnames(freq_num_sc_r)=="count"] <- "freq_num_sc"
+#colnames(freq_num_sc_r)[colnames(freq_num_sc_r)=="per"] <- "per_num_sc"
+
+
+#merge(freq_num_s_r, freq_num_sc_r, by=c("range", "total", "variable")
+)
--- src/sarah/msa_schev_vccs.R
+++ src/sarah/msa_schev_vccs.R
@@ -2,16 +2,14 @@ library(sf)
 library(tidyverse)
 library(tidyr)
 library(reshape2)
+library(data.table)
 
 #load MSA data from Census TIGER/Line Shapefiles
 msa <- st_read("src/sarah/tl_2017_us_cbsa/tl_2017_us_cbsa.shp")
-head(msa)
 st_crs(msa) #unprojected crs
 
 #filter for richmond and blacksburg MSAs
-r_msa <- filter(msa, NAME == "Richmond, VA")
-b_msa <- filter(msa, NAME == "Blacksburg-Christiansburg-Radford, VA")
-msa <- rbind(r_msa, b_msa)
+msa <- filter(msa, NAME %in% c("Richmond, VA","Blacksburg-Christiansburg-Radford, VA"))
 
 #load VCCS data
 vccs <- read.csv("data/stem_edu/working/ipeds_vccs/ipeds_vccs_cred.csv")
@@ -19,11 +17,9 @@ names(vccs)[names(vccs) == "College"] <- "institution"
 
 levels(vccs$institution)[levels(vccs$institution)=="J. Sargeant Reynolds Community College"] <- "J Sargeant Reynolds Community College"
 
-
 #filter to unique College, long and lat
 vccs <- vccs[c(2, 13:14)]
 vccs <- unique(vccs)
-head(vccs)
 
 #set same coordinates as msa data
 vccs <- vccs %>%
@@ -31,19 +27,12 @@ vccs <- vccs %>%
   st_set_crs(value = st_crs(x = msa))
 st_crs(vccs)
 
-
-
-#shows which VCCS colleges are in richmond, then blacksburg
-#r_msa_vccs <- st_join(x = r_msa, y = vccs)
-#b_msa_vccs <- st_join(x = b_msa, y = vccs)
-
 #load SCHEV STW Schools data
 stw_schools <- read.csv("data/stem_edu/working/ipeds_schev/ipeds_schev_stw_schools.csv")
 
 #filter to unique institution, long and lat
 stw_schools <- stw_schools[c(2, 13:14)]
 stw_schools <- unique(stw_schools)
-head(stw_schools)
 
 #set same coordinates as msa data
 stw_schools <- stw_schools %>%
@@ -51,19 +40,12 @@ stw_schools <- stw_schools %>%
   st_set_crs(value = st_crs(x = msa))
 st_crs(stw_schools)
 
-
-#shows which SCHEV STW SCHOOLS  are in richmond, then blacksburg
-#r_msa_stw_schools <- st_join(x = r_msa, y = stw_schools)
-#b_msa_stw_schools <- st_join(x = b_msa, y = stw_schools)
-
-
 #load SCHEV STW subbach combine data
 stw_subbach_combine <- read.csv("data/stem_edu/working/ipeds_schev/ipeds_schev_stw_subbach_combine.csv")
 
 #filter to unique institution, long and lat
 stw_subbach_combine <- stw_subbach_combine[c(2, 13:14)]
 stw_subbach_combine <- unique(stw_subbach_combine)
-head(stw_subbach_combine)
 
 #set same coordinates as msa data
 stw_subbach_combine <- stw_subbach_combine %>%
@@ -71,19 +53,12 @@ stw_subbach_combine <- stw_subbach_combine %>%
   st_set_crs(value = st_crs(x = msa))
 st_crs(stw_subbach_combine)
 
-
-
-#shows which SCHEV STW SUBBACH COMBINE  are in richmond, then blacksburg
-#r_msa_stw_subbach_combine <- st_join(x = r_msa, y = stw_subbach_combine)
-#b_msa_stw_subbach_combine <- st_join(x = b_msa, y = stw_subbach_combine)
-
 #load SCHEV  subbach combine data
 subbach_combine <- read.csv("data/stem_edu/working/ipeds_schev/ipeds_schev_subbach_combine.csv")
 
 #filter to unique institution, long and lat
 subbach_combine <- subbach_combine[c(2, 13:14)]
 subbach_combine <- unique(subbach_combine)
-head(subbach_combine)
 
 #set same coordinates as msa data
 subbach_combine <- subbach_combine %>%
@@ -91,17 +66,13 @@ subbach_combine <- subbach_combine %>%
   st_set_crs(value = st_crs(x = msa))
 st_crs(subbach_combine)
 
-
-#shows which SCHEV  SUBBACH COMBINE  are in richmond, then blacksburg
-#r_msa_subbach_combine <- st_join(x = r_msa, y = subbach_combine)
-#b_msa_subbach_combine <- st_join(x = b_msa, y = subbach_combine)
-
-
+#return schools only in Richmond or blacksburg
 msa_vccs <- st_join(x=msa, y=vccs)
 msa_stw_schools <- st_join(x=msa, y=stw_schools)
 msa_stw_subbach_combine <- st_join(x=msa, y=stw_subbach_combine)
 msa_subbach_combine <- st_join(x=msa, y=subbach_combine)
 
+#List of all schools in MSAs with MSA geometry
 msa_edu <- rbind(msa_vccs, msa_stw_schools, msa_stw_subbach_combine, msa_subbach_combine)
 msa_edu <- unique(msa_edu)
 msa_edu <- msa_edu %>% select(institution, geometry)
@@ -112,76 +83,27 @@ stw_subbach_combine$type = "stw_subbach_combine"
 subbach_combine$type = "subbach_combine"
 
 
-###COMBINE ALL
+###list of all schools in MSAs with point geometry
 edu <- rbind(stw_schools, subbach_combine, stw_subbach_combine, vccs)
 edu$value =TRUE
 edu <- spread(edu, type, value)
-library(data.table)
 edu <- setDT(edu)[institution %chin% msa_edu$institution]
-
 colnames(edu)[colnames(edu)=="institution"] <- "object"
 edu <- replace(edu, is.na(edu), FALSE)
 
+#MSA geometry only
 msa <- msa %>% select(NAME, geometry)
 colnames(msa)[colnames(msa)=="NAME"] <- "object"
 
-msa$stw_schools = NA
-msa$stw_subbach_combine = NA
-msa$subbach_combine = NA
-msa$VCCS = NA
-msa <- msa[,c(1,3,4,5,6,2)]
-
-p <-ggplot()+
-  geom_sf(data=st_va)+
-  geom_sf(data = msa, fill = NA) +
-  geom_sf(data = edu)
-
-
+#virginia shapefiles
 st <- st_read("src/sarah/tl_2017_us_state/tl_2017_us_state.shp")
 st_va <- filter(st, STUSPS == "VA")
 
-
-
-
-
-
-
-
-#br <- rbind(b, r)
-
-
-
-
-
-#b <- rbind(b_msa_stw_schools, b_msa_stw_subbach_combine, b_msa_subbach_combine)
-#b <- unique(b)
-
-#colnames(b)
-#colnames(b_msa_vccs)
-
-#b_msa_vccs <- b_msa_vccs[ -c(13, 15:29) ]
-
-#colnames(b)
-#colnames(b_msa_vccs)
-
-#b<- rbind(b, b_msa_vccs)
-#b <- unique(b)
-#head(b)
-
-#r<- rbind(r_msa_stw_schools, r_msa_stw_subbach_combine, r_msa_subbach_combine)
-#r <- unique(r)
-
-#colnames (r)
-#colnames(r_msa_vccs)
-
-#r_msa_vccs <- r_msa_vccs[ -c(13, 15:29) ]
-#colnames (r)
-#colnames(r_msa_vccs)
-
-#r <- rbind(r, r_msa_vccs)
-#r <- unique(r)
-#head(r)
-
+#plot of Virginia, MSAs, education within MSAs
+ggplot()+
+  geom_sf(data=st_va)+
+  geom_sf(data = msa, fill = NA) +
+  geom_sf(data = edu)
 
 
 
